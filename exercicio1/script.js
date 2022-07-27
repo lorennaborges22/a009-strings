0048468510dd4f6ddc92a2d6ca95717c4289be2e
Lorennaborges22 <loreborges1998@gmail.com>
2022-07-26 23:35:25 -0300

    atividade009

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nome = prompt("Qual seu nome?")
+const comida1 = prompt("Qual as sua comidas favorita?")
+const comida2 = prompt("mais umas favorita")
+const comida3 = prompt("a última favorita")
+
+
+console.log(`Estas são as comidas favoritas de ${nome} \n
+${comida1} \n${comida2} \n${comida3} \n`)
+
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim())
+
+//-----------
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.length)
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim().length)
+
+//-----
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhasStringMelhor = minhaString.replaceAll("_","sticioso")
+console.log(minhasStringMelhor)
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ____.      ";
+console.log(minhaString. replace(/____./g, "sticioso"))
